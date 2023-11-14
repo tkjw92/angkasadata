41247a7a8c26b739095e31671c2719a4ef96ac47
tkjw92 <muhammadnizarramli@gmail.com>
2023-11-14 22:02:54 +0700

    board name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,20 @@
         <div class="row">
 
             <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-secondary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
+                                    Board Name</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $resources['board-name'] }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
